2516c94471be92486524f55b7fbcf16d53f6a0c4
shekhar-2410 <shekhar.devanshee@gmail.com>
2022-03-15 10:57:26 +0530

    modal

---
+++ modal/modal.js
@@ -0,0 +1,22 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+
+const openModel = function() {
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) btnOpenModal[i].addEventListener('click', openModel);
+
+const closeModel = function() {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModel);
+
+overlay.addEventListener('click', closeModel);
